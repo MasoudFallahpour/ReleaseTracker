ff2b01cbe79736ad7895e79d3a0eeac9c4fd2a66
Masood <masood.fallahpoor@gmail.com>
2020-10-10 22:55:51 +0330

    Set versionName to 0.1

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,5 +4,5 @@ object Versions {
     const val targetSdkVersion = 30
     const val buildToolsVersion = "30.0.2"
     const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionName = "0.1"
 }
