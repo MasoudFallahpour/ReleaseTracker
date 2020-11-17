6c61d313a09172ec20cd6abc23efc61774765ae8
Masood <masood.fallahpoor@gmail.com>
2020-11-17 12:43:07 +0330

    Update Gradle and AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("com.android.tools.build:gradle:4.2.0-alpha16")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.28-alpha")
         classpath("com.google.gms:google-services:4.3.4")
