a3ce6bc4a08e0ec4882de0728b7154b8eb710419
masood <masood.fallahpoor@gmail.com>
2021-01-31 11:22:06 +0330

    Use Jetpack Compose for building the UI of NightModeDialog

--- app/src/main/java/ir/fallahpoor/releasetracker/libraries/view/dialogs/NightModeDialog.kt
+++ app/src/main/java/ir/fallahpoor/releasetracker/libraries/view/dialogs/NightModeDialog.kt
@@ -4,11 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import ir.fallahpoor.releasetracker.R
 import ir.fallahpoor.releasetracker.common.NightModeManager
+import ir.fallahpoor.releasetracker.common.SPACE_NORMAL
 import ir.fallahpoor.releasetracker.data.utils.LocalStorage
-import ir.fallahpoor.releasetracker.databinding.DialogNightModeBinding
+import ir.fallahpoor.releasetracker.theme.ReleaseTrackerTheme
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -24,43 +38,64 @@ class NightModeDialog : BaseBottomSheetDialogFragment() {
     @Inject
     lateinit var localStorage: LocalStorage
 
-    private var _binding: DialogNightModeBinding? = null
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = DialogNightModeBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setupViews()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
-
-    private fun setupViews() {
-        val currentMode = NightModeManager.Mode.valueOf(
-            localStorage.getNightMode() ?: NightModeManager.Mode.OFF.name
-        )
-        when (currentMode) {
-            NightModeManager.Mode.OFF -> binding.nightModeOffButton.isChecked = true
-            NightModeManager.Mode.ON -> binding.nightModeOnButton.isChecked = true
-            NightModeManager.Mode.AUTO -> binding.nightModeAutoButton.isChecked = true
-        }
-        binding.nightModeRadioGroup.setOnCheckedChangeListener { _, checkedId ->
-            when (checkedId) {
-                R.id.nightModeOffButton -> nightModeManager.setNightMode(NightModeManager.Mode.OFF)
-                R.id.nightModeOnButton -> nightModeManager.setNightMode(NightModeManager.Mode.ON)
-                R.id.nightModeAutoButton -> nightModeManager.setNightMode(NightModeManager.Mode.AUTO)
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            ReleaseTrackerTheme(darkTheme = nightModeManager.isDarkTheme()) {
+                val currentMode = NightModeManager.Mode.valueOf(
+                    localStorage.getNightMode() ?: NightModeManager.Mode.OFF.name
+                )
+                NightModeScreen(currentMode)
             }
+        }
+    }
+
+    @Composable
+    private fun NightModeScreen(currentMode: NightModeManager.Mode) {
+        Column {
+            Text(
+                text = stringResource(R.string.select_night_mode),
+                modifier = Modifier.padding(SPACE_NORMAL.dp)
+            )
+            NightModeItem(R.string.off, NightModeManager.Mode.OFF, currentMode)
+            NightModeItem(R.string.on, NightModeManager.Mode.ON, currentMode)
+            NightModeItem(R.string.auto, NightModeManager.Mode.AUTO, currentMode)
+        }
+    }
+
+    @Composable
+    private fun NightModeItem(
+        @StringRes text: Int,
+        mode: NightModeManager.Mode,
+        currentMode: NightModeManager.Mode
+    ) {
+        val clickListener = {
+            nightModeManager.setNightMode(mode)
             dismiss()
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable(onClick = clickListener)
+        ) {
+            RadioButton(
+                selected = mode == currentMode,
+                onClick = clickListener,
+                modifier = Modifier.padding(SPACE_NORMAL.dp)
+            )
+            Text(
+                text = stringResource(text),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = SPACE_NORMAL.dp,
+                        bottom = SPACE_NORMAL.dp,
+                        end = SPACE_NORMAL.dp
+                    )
+            )
         }
     }
 
