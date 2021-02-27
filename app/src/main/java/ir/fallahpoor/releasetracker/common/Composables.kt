d399b4b6af4785c9e0a9d32530b9aa364f0be403
Masood <masood.fallahpoor@gmail.com>
2021-02-27 12:14:06 +0330

    Remove the padding of Snackbar

--- app/src/main/java/ir/fallahpoor/releasetracker/common/Composables.kt
+++ app/src/main/java/ir/fallahpoor/releasetracker/common/Composables.kt
@@ -1,13 +1,11 @@
 package ir.fallahpoor.releasetracker.common
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Snackbar
 import androidx.compose.material.SnackbarData
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun DefaultSnackbar(
@@ -18,12 +16,7 @@ fun DefaultSnackbar(
         hostState = snackbarHostState,
         snackbar = { snackbarData: SnackbarData ->
             Snackbar(
-                snackbarData = snackbarData,
-                modifier = Modifier.padding(
-                    bottom = SPACE_NORMAL.dp,
-                    start = SPACE_NORMAL.dp,
-                    end = SPACE_NORMAL.dp
-                )
+                snackbarData = snackbarData
             )
         },
         modifier = modifier
