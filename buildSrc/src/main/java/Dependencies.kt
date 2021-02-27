0545393d8fd8fea23fcc57b72e855fa625c9d74c
Masood <masood.fallahpoor@gmail.com>
2021-02-27 12:08:29 +0330

    Update some dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -12,8 +12,8 @@ object Dependencies {
         const val core = Common.core
         const val preference = "androidx.preference:preference-ktx:1.1.0"
         const val appCompat = "androidx.appcompat:appcompat:1.2.0"
-        const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha02"
-        const val navigationCompose = "androidx.navigation:navigation-compose:1.0.0-alpha07"
+        const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha03"
+        const val navigationCompose = "androidx.navigation:navigation-compose:1.0.0-alpha08"
         const val material = "com.google.android.material:material:1.3.0-rc01"
         const val hilt = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
         const val hiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hiltVersion}"
