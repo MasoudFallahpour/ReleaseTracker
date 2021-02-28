3454b68d830941f03cac86fdeb367b087421f51f
Masood <masood.fallahpoor@gmail.com>
2021-02-28 22:33:40 +0330

    Add SearchBar composable

--- app/src/main/java/ir/fallahpoor/releasetracker/common/Composables.kt
+++ app/src/main/java/ir/fallahpoor/releasetracker/common/Composables.kt
@@ -1,11 +1,16 @@
 package ir.fallahpoor.releasetracker.common
 
-import androidx.compose.material.Snackbar
-import androidx.compose.material.SnackbarData
-import androidx.compose.material.SnackbarHost
-import androidx.compose.material.SnackbarHostState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import ir.fallahpoor.releasetracker.theme.ReleaseTrackerTheme
 
 @Composable
 fun DefaultSnackbar(
@@ -22,3 +27,80 @@ fun DefaultSnackbar(
         modifier = modifier
     )
 }
+
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    hint: String,
+    query: String,
+    onQueryChange: (String) -> Unit,
+    onQuerySubmit: (String) -> Unit,
+    onClearClick: () -> Unit,
+) {
+    TextField(
+        modifier = modifier,
+        value = query,
+        onValueChange = onQueryChange,
+        placeholder = {
+            Text(
+                text = hint
+            )
+        },
+        singleLine = true,
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = null
+            )
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = onClearClick
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Clear,
+                    contentDescription = null
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onQuerySubmit(query)
+            }
+        )
+    )
+}
+
+@Composable
+@Preview
+private fun SearchBarPreview() {
+    ReleaseTrackerTheme {
+        Surface {
+            SearchBar(
+                hint = "Search",
+                query = "Coil",
+                onQueryChange = {},
+                onClearClick = {},
+                onQuerySubmit = {}
+            )
+        }
+    }
+}
+
+//SearchBar(
+//modifier = Modifier.fillMaxWidth(),
+//hint = stringResource(R.string.search),
+//query = searchQuery,
+//onQueryChange = {
+//    searchQuery = it
+//    librariesViewModel.getLibraries(searchTerm = it)
+//},
+//onQuerySubmit = {
+//    librariesViewModel.getLibraries(searchTerm = it)
+//},
+//onClearClick = {
+//    searchQuery = ""
+//    librariesViewModel.getLibraries(searchTerm = "")
+//}
+//)
