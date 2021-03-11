b3256aae34ed1da305ced6c7111fac86423277bd
Masood <masood.fallahpoor@gmail.com>
2021-03-11 17:54:37 +0330

    Add FakeStorage

---
+++ data/src/test/java/ir/fallahpoor/releasetracker/data/fakes/FakeStorage.kt
@@ -0,0 +1,41 @@
+package ir.fallahpoor.releasetracker.data.fakes
+
+import ir.fallahpoor.releasetracker.data.utils.NightMode
+import ir.fallahpoor.releasetracker.data.utils.SortOrder
+import ir.fallahpoor.releasetracker.data.utils.storage.Storage
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeStorage : Storage {
+
+    private var sortOrder = SortOrder.A_TO_Z
+    private var lastUpdateCheckDate = "N/A"
+    private var nightMode = NightMode.AUTO
+
+    override fun setSortOrder(sortOrder: SortOrder) {
+        this.sortOrder = sortOrder
+    }
+
+    override fun getSortOrder() = sortOrder
+
+    override fun setLastUpdateCheck(date: String) {
+        lastUpdateCheckDate = date
+    }
+
+    override fun getLastUpdateCheck(): Flow<String> =
+        flow {
+            lastUpdateCheckDate
+        }
+
+    override fun getNightModeAsFlow(): Flow<NightMode> =
+        flow {
+            nightMode
+        }
+
+    override fun getNightMode() = nightMode
+
+    override fun setNightMode(nightMode: NightMode) {
+        this.nightMode = nightMode
+    }
+
+}
