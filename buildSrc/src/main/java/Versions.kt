7832a17679a1fc7843cba9c760e47e45763dd8f1
Masood <masood.fallahpoor@gmail.com>
2021-05-18 13:36:24 +0200

    Update Compose to beta06

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -9,7 +9,7 @@ object Versions {
     const val versionName = "0.1"
 
     const val kotlinVersion = "1.4.32"
-    const val composeVersion = "1.0.0-beta05"
+    const val composeVersion = "1.0.0-beta06"
     const val hiltVersion = "2.35"
     const val hiltJetpackVersion = "1.0.0"
     const val roomVersion = "2.3.0"
