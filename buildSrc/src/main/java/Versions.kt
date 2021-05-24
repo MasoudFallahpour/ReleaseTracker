0783084ef8a0bb24d4a683ec08649669e9f97ba1
Masood <masood.fallahpoor@gmail.com>
2021-05-24 16:32:37 +0200

    Upgrade Hilt to 2.35.1

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
 
     const val kotlinVersion = "1.4.32"
     const val composeVersion = "1.0.0-beta06"
-    const val hiltVersion = "2.35"
+    const val hiltVersion = "2.35.1"
     const val hiltJetpackVersion = "1.0.0"
     const val roomVersion = "2.3.0"
     const val retrofitVersion = "2.9.0"
