213b6feab37d807a6bfc9fa66b2e81224d042ce8
Masood <masood.fallahpoor@gmail.com>
2021-05-27 12:00:36 +0200

    Bump versionCode and versionName

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,8 +3,8 @@ object Versions {
     const val minSdkVersion = 21
     const val targetSdkVersion = 30
 
-    const val versionCode = 1
-    const val versionName = "0.1"
+    const val versionCode = 3
+    const val versionName = "0.3"
 
     const val kotlinVersion = "1.4.32"
     const val composeVersion = "1.0.0-beta06"
