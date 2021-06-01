2054e11d7a510053380d43ed33cbfd47c0c791cc
Masood <masood.fallahpoor@gmail.com>
2021-06-01 11:19:02 +0200

    Make the tests of AddLibraryViewModel more comprehensive

--- app/src/test/java/ir/fallahpoor/releasetracker/addlibrary/viewmodel/AddLibraryViewModelTest.kt
+++ app/src/test/java/ir/fallahpoor/releasetracker/addlibrary/viewmodel/AddLibraryViewModelTest.kt
@@ -7,6 +7,7 @@ import ir.fallahpoor.releasetracker.addlibrary.view.AddLibraryState
 import ir.fallahpoor.releasetracker.data.utils.ExceptionParser
 import ir.fallahpoor.releasetracker.fakes.FakeLibraryRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,99 +27,115 @@ class AddLibraryViewModelTest {
     val mainCoroutineScopeRule = MainCoroutineScopeRule()
 
     private lateinit var addLibraryViewModel: AddLibraryViewModel
+    private lateinit var fakeLibraryRepository: FakeLibraryRepository
 
     @Before
     fun runBeforeEachTest() {
+        fakeLibraryRepository = FakeLibraryRepository()
         addLibraryViewModel = AddLibraryViewModel(
-            libraryRepository = FakeLibraryRepository(),
+            libraryRepository = fakeLibraryRepository,
             exceptionParser = ExceptionParser()
         )
     }
 
     @Test
-    fun `addLibrary() should set the state to EmptyLibraryName when library name is empty`() {
+    fun `addLibrary() should set the state to EmptyLibraryName when library name is empty`() =
+        runBlockingTest {
 
-        // Given
+            // Given
 
-        // When
-        addLibraryViewModel.addLibrary(
-            libraryName = "",
-            libraryUrl = LIBRARY_URL
-        )
+            // When
+            addLibraryViewModel.addLibrary(
+                libraryName = "",
+                libraryUrl = LIBRARY_URL
+            )
 
-        // Then
-        Truth.assertThat(addLibraryViewModel)
-        Truth.assertThat(addLibraryViewModel.state.value)
-            .isInstanceOf(AddLibraryState.EmptyLibraryName::class.java)
+            // Then
+            Truth.assertThat(fakeLibraryRepository.getLibraries().isEmpty()).isTrue()
+            Truth.assertThat(addLibraryViewModel.state.value)
+                .isInstanceOf(AddLibraryState.EmptyLibraryName::class.java)
 
-    }
-
-    @Test
-    fun `addLibrary() should set the state to EmptyLibraryUrl when library URL is empty`() {
-
-        // Given
-
-        // When
-        addLibraryViewModel.addLibrary(
-            libraryName = LIBRARY_NAME,
-            libraryUrl = ""
-        )
-
-        // Then
-        Truth.assertThat(addLibraryViewModel.state.value)
-            .isInstanceOf(AddLibraryState.EmptyLibraryUrl::class.java)
-
-    }
+        }
 
     @Test
-    fun `addLibrary() should set the state to InvalidLibraryUrl when library URL is invalid`() {
+    fun `addLibrary() should set the state to EmptyLibraryUrl when library URL is empty`() =
+        runBlockingTest {
 
-        // Given
+            // Given
 
-        // When
-        addLibraryViewModel.addLibrary(
-            libraryName = LIBRARY_NAME,
-            libraryUrl = "InvalidLibraryUrl"
-        )
+            // When
+            addLibraryViewModel.addLibrary(
+                libraryName = LIBRARY_NAME,
+                libraryUrl = ""
+            )
 
-        // Then
-        Truth.assertThat(addLibraryViewModel.state.value)
-            .isInstanceOf(AddLibraryState.InvalidLibraryUrl::class.java)
+            // Then
+            Truth.assertThat(fakeLibraryRepository.getLibraries().isEmpty()).isTrue()
+            Truth.assertThat(addLibraryViewModel.state.value)
+                .isInstanceOf(AddLibraryState.EmptyLibraryUrl::class.java)
 
-    }
-
-    @Test
-    fun `addLibrary() should set the state to Error when library name already exists`() {
-
-        // Given
-
-        // When
-        addLibraryViewModel.addLibrary(
-            libraryName = LIBRARY_NAME,
-            libraryUrl = LIBRARY_URL
-        )
-
-        // Then
-        Truth.assertThat(addLibraryViewModel.state.value)
-            .isInstanceOf(AddLibraryState.Error::class.java)
-
-    }
+        }
 
     @Test
-    fun `addLibrary() should set the state to LibraryAdded when library is added successfully`() {
+    fun `addLibrary() should set the state to InvalidLibraryUrl when library URL is invalid`() =
+        runBlockingTest {
 
-        // Given
+            // Given
 
-        // When
-        addLibraryViewModel.addLibrary(
-            libraryName = "Coil",
-            libraryUrl = LIBRARY_URL
-        )
+            // When
+            addLibraryViewModel.addLibrary(
+                libraryName = LIBRARY_NAME,
+                libraryUrl = "InvalidLibraryUrl"
+            )
 
-        // Then
-        Truth.assertThat(addLibraryViewModel.state.value)
-            .isInstanceOf(AddLibraryState.LibraryAdded::class.java)
+            // Then
+            Truth.assertThat(fakeLibraryRepository.getLibraries().isEmpty()).isTrue()
+            Truth.assertThat(addLibraryViewModel.state.value)
+                .isInstanceOf(AddLibraryState.InvalidLibraryUrl::class.java)
 
-    }
+        }
+
+    @Test
+    fun `addLibrary() should set the state to Error when library name already exists`() =
+        runBlockingTest {
+
+            // Given
+            fakeLibraryRepository.addLibrary(
+                libraryName = LIBRARY_NAME,
+                libraryUrl = LIBRARY_URL,
+                libraryVersion = "version1"
+            )
+
+            // When
+            addLibraryViewModel.addLibrary(
+                libraryName = LIBRARY_NAME,
+                libraryUrl = LIBRARY_URL
+            )
+
+            // Then
+            Truth.assertThat(fakeLibraryRepository.getLibraries().size).isEqualTo(1)
+            Truth.assertThat(addLibraryViewModel.state.value)
+                .isInstanceOf(AddLibraryState.Error::class.java)
+
+        }
+
+    @Test
+    fun `addLibrary() should set the state to LibraryAdded when library is added successfully`() =
+        runBlockingTest {
+
+            // Given
+
+            // When
+            addLibraryViewModel.addLibrary(
+                libraryName = LIBRARY_NAME,
+                libraryUrl = LIBRARY_URL
+            )
+
+            // Then
+            Truth.assertThat(fakeLibraryRepository.getLibrary(LIBRARY_NAME)).isNotNull()
+            Truth.assertThat(addLibraryViewModel.state.value)
+                .isInstanceOf(AddLibraryState.LibraryAdded::class.java)
+
+        }
 
 }
--- app/src/test/java/ir/fallahpoor/releasetracker/fakes/FakeLibraryRepository.kt
+++ app/src/test/java/ir/fallahpoor/releasetracker/fakes/FakeLibraryRepository.kt
@@ -8,51 +8,51 @@ import kotlinx.coroutines.flow.Flow
 class FakeLibraryRepository : LibraryRepository {
 
     companion object {
-        const val EXISTING_LIBRARY_NAME = "ReleaseTracker"
         const val LIBRARY_VERSION = "0.2"
     }
+
+    private val libraries = mutableListOf<Library>()
 
     override suspend fun addLibrary(
         libraryName: String,
         libraryUrl: String,
         libraryVersion: String
     ) {
+        val library: Library? = libraries.find { it.name == libraryName }
+        if (library != null) {
+            throw RuntimeException()
+        } else {
+            libraries.add(Library(libraryName, libraryUrl, libraryVersion, 0))
+        }
     }
 
     override suspend fun updateLibrary(library: Library) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getLibrary(libraryName: String): Library? {
-        return if (libraryName == EXISTING_LIBRARY_NAME) {
-            Library(
-                name = "ReleaseTracker",
-                url = "https://github.com/masoodfallahpoor/ReleaseTracker",
-                version = LIBRARY_VERSION
-            )
-        } else {
-            null
+        val removed: Boolean = libraries.removeIf { it.name == library.name }
+        if (removed) {
+            libraries.add(library)
         }
     }
+
+    override suspend fun getLibrary(libraryName: String): Library? =
+        libraries.firstOrNull { it.name == libraryName }
 
     override fun getLibraries(sortOrder: SortOrder, searchTerm: String): Flow<List<Library>> {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getLibraries(): List<Library> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getLibraries(): List<Library> = libraries
 
     override suspend fun deleteLibrary(library: Library) {
-        TODO("Not yet implemented")
+        libraries.removeIf { it.name == library.name }
     }
 
     override suspend fun getLibraryVersion(libraryName: String, libraryUrl: String): String {
+        // TODO Correct the implementation
         return LIBRARY_VERSION
     }
 
     override suspend fun pinLibrary(library: Library, pinned: Boolean) {
-        TODO("Not yet implemented")
+        updateLibrary(library.copy(pinned = if (pinned) 1 else 0))
     }
 
     override fun getLastUpdateCheck(): Flow<String> {
