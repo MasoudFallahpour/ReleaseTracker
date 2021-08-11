8a70a6691f02a1836a7795f3e4b2c087c3a4882d
Masood <masood.fallahpoor@gmail.com>
2021-08-11 15:13:26 +0200

    Update Compose to 1.0.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val versionCode = 3
     const val versionName = "0.3"
 
-    const val kotlin = "1.5.10"
+    const val kotlin = "1.5.21"
     const val appCompat = "1.3.0"
     const val core = "1.5.0"
     const val room = "2.3.0"
@@ -15,9 +15,9 @@ object Versions {
     const val preference = "1.1.1"
     const val firebase = "28.3.0"
     const val material = "1.4.0"
-    const val compose = "1.0.0"
-    const val activityCompose = "1.3.0"
-    const val navigationCompose = "2.4.0-alpha05"
+    const val compose = "1.0.1"
+    const val activityCompose = "1.3.1"
+    const val navigationCompose = "2.4.0-alpha06"
     const val hilt = "2.38.1"
     const val hiltJetpack = "1.0.0"
     const val hiltNavigationCompose = "1.0.0-alpha03"
