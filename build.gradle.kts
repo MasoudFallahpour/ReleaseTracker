0922bafc5e6f9d1cbc99ce283886fd58655aa051
Masood <masood.fallahpoor@gmail.com>
2021-09-18 16:08:31 +0200

    Update Gradle and some of the Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta02")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha12")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Hilt.version}")
-        classpath("com.google.gms:google-services:4.3.8")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.0")
+        classpath("com.google.gms:google-services:4.3.10")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
         classpath("org.jacoco:org.jacoco.core:$jacocoVersion")
     }
 }
