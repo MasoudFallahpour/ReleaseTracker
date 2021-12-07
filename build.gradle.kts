0b0e9601789247beb4e8134db8e855b400430554
Masood <masood.fallahpoor@gmail.com>
2021-12-07 16:14:17 +0100

    Upgrade Crashlytics' Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Hilt.version}")
         classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.7.1")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
         classpath("org.jacoco:org.jacoco.core:$jacocoVersion")
     }
 }
