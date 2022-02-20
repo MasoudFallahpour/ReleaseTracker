2050c2f870f6043a187260825f52a9713daa574e
Masood <masood.fallahpoor@gmail.com>
2022-02-20 18:21:10 +0100

    Update the tests of AddLibraryScreen composable

--- app/src/androidTest/kotlin/ir/fallahpoor/releasetracker/addlibrary/view/AddLibraryScreenTest.kt
+++ app/src/androidTest/kotlin/ir/fallahpoor/releasetracker/addlibrary/view/AddLibraryScreenTest.kt
@@ -1,23 +1,20 @@
 package ir.fallahpoor.releasetracker.addlibrary.view
 
 import android.content.Context
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
+import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ApplicationProvider
 import ir.fallahpoor.releasetracker.R
 import ir.fallahpoor.releasetracker.addlibrary.viewmodel.AddLibraryViewModel
-import ir.fallahpoor.releasetracker.data.utils.ExceptionParser
-import ir.fallahpoor.releasetracker.fakes.FakeLibraryRepository
+import ir.fallahpoor.releasetracker.common.GITHUB_BASE_URL
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
-import org.mockito.kotlin.mock
 
 @RunWith(MockitoJUnitRunner::class)
 class AddLibraryScreenTest {
@@ -25,10 +22,8 @@ class AddLibraryScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @get:Rule
-    val instantTaskExecutorRule = InstantTaskExecutorRule()
-
-    private val context: Context = ApplicationProvider.getApplicationContext()
+    @Mock
+    private lateinit var addLibraryViewModel: AddLibraryViewModel
 
     @Test
     fun screen_is_initialized_correctly() {
@@ -51,9 +46,93 @@ class AddLibraryScreenTest {
     }
 
     @Test
+    fun correct_library_name_is_displayed() {
+
+        // Given
+        initializeAddLibraryScreen(libraryName = "Coil")
+
+        // Then
+        composeTestRule.onNodeWithTag(
+            AddLibraryTags.LIBRARY_NAME_TEXT_FIELD,
+            useUnmergedTree = true
+        ).assertTextEquals("Coil")
+
+    }
+
+    @Test
+    fun correct_library_url_is_displayed() {
+
+        // Given
+        initializeAddLibraryScreen(libraryUrl = "coil-kt/coil")
+
+        // Then
+        composeTestRule.onNodeWithTag(
+            AddLibraryTags.LIBRARY_URL_TEXT_FIELD,
+            useUnmergedTree = true
+        )
+            .assertTextEquals(GITHUB_BASE_URL + "coil-kt/coil")
+
+    }
+
+    @Test
+    fun libraryName_is_updated_when_a_library_name_is_entered() {
+
+        // Given
+        initializeAddLibraryScreen()
+
+        // When
+        composeTestRule.onNodeWithTag(AddLibraryTags.LIBRARY_NAME_TEXT_FIELD)
+            .performTextInput("Coil")
+
+        // Then
+        Mockito.verify(addLibraryViewModel).libraryName = "Coil"
+
+    }
+
+    @Test
+    fun libraryUrlPath_is_updated_when_a_library_URL_is_entered() {
+
+        // Given
+        initializeAddLibraryScreen()
+
+        // When
+        composeTestRule.onNodeWithTag(AddLibraryTags.LIBRARY_URL_TEXT_FIELD)
+            .performTextInput("coil-kt/coil")
+
+        // Then
+        Mockito.verify(addLibraryViewModel).libraryUrlPath = "coil-kt/coil"
+
+    }
+
+    @Test
+    fun addLibrary_is_called_when_adding_a_new_library() {
+
+        // Given
+        initializeAddLibraryScreen()
+
+        // When
+        with(composeTestRule) {
+            onNodeWithTag(AddLibraryTags.LIBRARY_NAME_TEXT_FIELD)
+                .performTextInput("Coil")
+            onNodeWithTag(AddLibraryTags.LIBRARY_URL_TEXT_FIELD)
+                .performTextInput("coil-kt/coil")
+            onNodeWithTag(AddLibraryTags.ADD_LIBRARY_BUTTON)
+                .performClick()
+        }
+
+        // Then
+        // TODO assert for the exact library name and URL not just any string
+        Mockito.verify(addLibraryViewModel).addLibrary(anyString(), anyString())
+
+    }
+
+    // TODO Add a test to assert that AddLibraryViewModel.resetState() is called when dismissing an error
+
+    @Test
     fun correct_callback_is_called_when_pressing_the_back_button() {
 
         // Given
+        val context: Context = ApplicationProvider.getApplicationContext()
         val onBackClick: () -> Unit = mock()
         initializeAddLibraryScreen(onBackClick = onBackClick)
 
@@ -66,17 +145,24 @@ class AddLibraryScreenTest {
 
     }
 
-    private fun initializeAddLibraryScreen(onBackClick: () -> Unit = {}) {
+    private fun initializeAddLibraryScreen(
+        state: AddLibraryState = AddLibraryState.Initial,
+        libraryName: String = "",
+        libraryUrl: String = "",
+        onBackClick: () -> Unit = {}
+    ) {
+        Mockito.`when`(addLibraryViewModel.state).thenReturn(MutableLiveData(state))
+        Mockito.`when`(addLibraryViewModel.libraryName).thenReturn(libraryName)
+        Mockito.`when`(addLibraryViewModel.libraryUrlPath).thenReturn(libraryUrl)
         composeTestRule.setContent {
             AddLibraryScreen(
-                addLibraryViewModel = AddLibraryViewModel(
-                    FakeLibraryRepository(),
-                    ExceptionParser()
-                ),
+                addLibraryViewModel = addLibraryViewModel,
                 isDarkTheme = false,
                 onBackClick = onBackClick
             )
         }
     }
+
+    private inline fun <reified T : Any> mock(): T = Mockito.mock(T::class.java)
 
 }
