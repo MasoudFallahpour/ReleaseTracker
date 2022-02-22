b1acbda6454382c926c44de7f72ccd0ece53d1be
Masood <masood.fallahpoor@gmail.com>
2022-02-22 21:58:25 +0100

    Put AddLibraryContent composable and all its dependencies into its own dedicated file

---
+++ app/src/main/kotlin/ir/fallahpoor/releasetracker/addlibrary/ui/AddLibraryTags.kt
@@ -0,0 +1,13 @@
+package ir.fallahpoor.releasetracker.addlibrary.ui
+
+object AddLibraryTags {
+    const val SCREEN = "screen"
+    const val CONTENT = "content"
+    const val TITLE = "title"
+    const val BACK_BUTTON = "backButton"
+    const val PROGRESS_INDICATOR = "progressIndicator"
+    const val LIBRARY_NAME_TEXT_FIELD = "libraryNameTextField"
+    const val LIBRARY_URL_TEXT_FIELD = "libraryUrlTextField"
+    const val ADD_LIBRARY_BUTTON = "addLibraryButton"
+    const val ADD_LIBRARY_BUTTON_TEXT = "addLibraryButtonText"
+}
