e1246031b87b79ffd7533b2090a5f1fa06de4ce0
Masood <masood.fallahpoor@gmail.com>
2022-05-25 19:12:55 +0200

    Refactor LibrariesViewModel

--- app/src/main/kotlin/ir/fallahpoor/releasetracker/libraries/LibrariesViewModel.kt
+++ app/src/main/kotlin/ir/fallahpoor/releasetracker/libraries/LibrariesViewModel.kt
@@ -11,6 +11,7 @@ import ir.fallahpoor.releasetracker.data.utils.SortOrder
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.util.*
 import javax.inject.Inject
 
@@ -26,39 +27,28 @@ class LibrariesViewModel
     )
 
     private val searchQueryFlow = MutableStateFlow("")
-    private val triggerFlow: Flow<Params> = libraryRepository.getSortOrderAsFlow()
-        .combine(searchQueryFlow) { sortOrder: SortOrder, searchQuery: String ->
-            Params(sortOrder, searchQuery)
-        }
-
-    val uiState: StateFlow<LibrariesListScreenUiState> = triggerFlow.distinctUntilChanged()
-        .flatMapLatest { params ->
-            libraryRepository.getLibrariesAsFlow()
-                .map { libraries ->
-                    libraries.filter {
-                        it.name.contains(params.searchQuery, ignoreCase = true)
-                    }
-                }
-                .map { libraries ->
-                    libraries.sort(params.sortOrder)
-                }
-        }.map { libraries ->
-            LibrariesListScreenUiState(
-                sortOrder = libraryRepository.getSortOrder(),
-                searchQuery = searchQueryFlow.value,
-                librariesListState = LibrariesListState.LibrariesLoaded(libraries)
-            )
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.Eagerly,
-            initialValue = LibrariesListScreenUiState(sortOrder = libraryRepository.getSortOrder())
-        )
-
-    private fun List<Library>.sort(sortOrder: SortOrder): List<Library> = when (sortOrder) {
-        SortOrder.A_TO_Z -> sortedBy { it.name.lowercase(Locale.getDefault()) }
-        SortOrder.Z_TO_A -> sortedByDescending { it.name.lowercase(Locale.getDefault()) }
-        SortOrder.PINNED_FIRST -> sortedByDescending { it.pinned }
+    private val getLibrariesTriggerFlow: Flow<Params> = combine(
+        libraryRepository.getSortOrderAsFlow(),
+        searchQueryFlow
+    ) { sortOrder: SortOrder, searchQuery: String ->
+        Params(sortOrder, searchQuery)
     }
+
+    val uiState: StateFlow<LibrariesListScreenUiState> =
+        getLibrariesTriggerFlow.distinctUntilChanged()
+            .flatMapLatest { params -> getLibrariesAsFlow(params) }
+            .map { libraries ->
+                LibrariesListScreenUiState(
+                    sortOrder = libraryRepository.getSortOrder(),
+                    searchQuery = searchQueryFlow.value,
+                    librariesListState = LibrariesListState.LibrariesLoaded(libraries)
+                )
+            }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.Eagerly,
+                initialValue = LibrariesListScreenUiState(sortOrder = libraryRepository.getSortOrder())
+            )
 
     val lastUpdateCheck: StateFlow<String> = libraryRepository.getLastUpdateCheck()
         .stateIn(scope = viewModelScope, started = SharingStarted.Eagerly, initialValue = "N/A")
@@ -68,7 +58,7 @@ class LibrariesViewModel
             is Event.PinLibrary -> pinLibrary(event.library, event.pin)
             is Event.DeleteLibrary -> deleteLibrary(event.library)
             is Event.ChangeSortOrder -> changeSortOrder(event.sortOrder)
-            is Event.ChangeSearchQuery -> changeSearchQuery(event.searchQuery)
+            is Event.ChangeSearchQuery -> searchQueryFlow.value = event.searchQuery
         }
     }
 
@@ -76,7 +66,8 @@ class LibrariesViewModel
         viewModelScope.launch {
             try {
                 libraryRepository.pinLibrary(library, pin)
-            } catch (_: Throwable) {
+            } catch (t: Throwable) {
+                Timber.e(t)
             }
         }
     }
@@ -85,7 +76,8 @@ class LibrariesViewModel
         viewModelScope.launch {
             try {
                 libraryRepository.deleteLibrary(library)
-            } catch (_: Throwable) {
+            } catch (t: Throwable) {
+                Timber.e(t)
             }
         }
     }
@@ -94,13 +86,27 @@ class LibrariesViewModel
         viewModelScope.launch {
             try {
                 libraryRepository.setSortOrder(sortOrder)
-            } catch (_: Throwable) {
+            } catch (t: Throwable) {
+                Timber.e(t)
             }
         }
     }
 
-    private fun changeSearchQuery(searchQuery: String) {
-        searchQueryFlow.value = searchQuery
+    private fun getLibrariesAsFlow(params: Params): Flow<List<Library>> =
+        libraryRepository.getLibrariesAsFlow()
+            .map { libraries ->
+                libraries.filter {
+                    it.name.contains(params.searchQuery, ignoreCase = true)
+                }
+            }
+            .map { libraries ->
+                libraries.sort(params.sortOrder)
+            }
+
+    private fun List<Library>.sort(sortOrder: SortOrder): List<Library> = when (sortOrder) {
+        SortOrder.A_TO_Z -> sortedBy { it.name.lowercase(Locale.getDefault()) }
+        SortOrder.Z_TO_A -> sortedByDescending { it.name.lowercase(Locale.getDefault()) }
+        SortOrder.PINNED_FIRST -> sortedByDescending { it.pinned }
     }
 
 }
